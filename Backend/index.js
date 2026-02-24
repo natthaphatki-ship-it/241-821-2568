6718175e3bc8993983d390b435b62d7f6d6d4556
natthaphatki-ship-it <natthaphat.ki@ku.th>
2026-02-24 12:42:36 +0700

    Database UP

--- Backend/index.js
+++ Backend/index.js
@@ -1,69 +1,76 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-
+const mysql = require('mysql2/promise')
 const port = 8000
-
 app.use(bodyParser.json())
-
 let users = []
 let counter = 1
+
+let conn = null
+const initDBconnection = async () => {
+     conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8821
+    })
+}
+
+//path = Get //users สำหรับ เก็บข้อมูล user ทั้งหมดที่มีอยู่ในระบบ
+app.get('/users', async (req, res) => {
+ const result = await conn.query('SELECT * FROM users')
+    res.json(result[0])
+
+})
+
+
 // parth = GET /test
-app.get('/users',(req,res) => {
+app.get('/users', (req, res) => {
     res.json(users)
 })
 
 // parth = POST /user
-app.post('/user',(req,res) => {
+app.post('/users', async (req, res) => {
     let user = req.body
-    user.id = counter++
-    users.push(user)
+    const results = await conn.query('INSERT INTO users SET ?', user)
+    console.log('results', results)
     res.json({
-        message: 'User added successfully',
-        user: user })
-})
-
-// parth = put /user/:id
-app.put('/user/:id', (req,res) => {
-  let id = req.params.id
-  let updatedUser = req.body
-  //หา user ที่มี id ตรงกับ id ที่ส่งมา
-  let selecetedIndex = users.findIndex(user => user.id == id)
-
-  //อัพเดตข้อมูลของ user นั้นด้วยข้อมูลที่ส่งมาใน body ของ request
-   if (selecetedIndex.name) {
-    users[selecetedIndex].name = updatedUser.name
-   }
-   if (selecetedIndex.email) {
-    users[selecetedIndex].email = updatedUser.email
-   }
-
-
-  //ส่ง response กลับไปว่าอัพเดตสำเร็จแล้ว
-  res.json ({
-    message: 'User updated successfully',
-    data: {
-        user: updatedUser,
-        indexUpdated: selecetedIndex
-    }
+        message: 'User created successfully',
+        data: results[0]
+    
     })
 })
 
-//path = Delete /user/:id
-app.delete('/user/:id', (req,res) => {
+// par th = put /user/:id
+app.put('/user/:id', (req, res) => {
     let id = req.params.id
-//user จาก id ที่ส่งมา
-   let selecetedIndex = users.findIndex(user => user.id == id)
+    let updatedUser = req.body
+    //หา user ที่มี id ตรงกับ id ที่ส่งมา
+    let selecetedIndex = users.findIndex(user => user.id == id)
 
-   users.splice[selecetedIndex]
+    //อัพเดตข้อมูลของ user นั้นด้วยข้อมูลที่ส่งมาใน body ของ request
+    if (selecetedIndex.name) {
+        users[selecetedIndex].name = updatedUser.name
+    }
+    if (selecetedIndex.email) {
+        users[selecetedIndex].email = updatedUser.email
+    }
 
 
+    //ส่ง response กลับไปว่าอัพเดตสำเร็จแล้ว
     res.json({
-        message: 'User deleted successfully',
-         indexDeleted: selecetedIndex
+        message: 'User updated successfully',
+        data: {
+            user: updatedUser,
+            indexUpdated: selecetedIndex
+        }
     })
 })
 
-app.listen(port, () => {
+
+app.listen(port, async () => {
+    await initDBconnection()
     console.log(`Server runing is on port ${port}`)
 })
